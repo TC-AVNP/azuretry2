2c1e3bb23bcd6d717c7552396b7e88218b70ec2b
Pedro Condeco <pedro.condeco@freiheit.com>
2020-04-07 17:29:14 +0100

    mata tudo

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:sales_academy/app.dart';
-
-void main() {
-  testWidgets('renders the app container', (WidgetTester tester) async {
-    await tester.pumpWidget(LRApp());
-    expect(find.byType(LRApp), findsOneWidget);
-  });
-}
